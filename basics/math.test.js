84d84691ed124a2ca685831ffb04553cf9e6c2df
Shomail <shomail1988@gmail.com>
2019-01-17 23:44:29 +0100

    added support for async code testing

--- basics/math.test.js
+++ basics/math.test.js
@@ -1,6 +1,6 @@
-const { test } = require('./testingFramework');
+const { test, asyncTest } = require('./testingFramework');
 const { expect } = require('./assertionLibrary');
-const { sum, subtract } = require('./math');
+const { sum, subtract, sumAsync, subtractAsync } = require('./math');
 
 console.log('Testing math.js functions');
 
@@ -13,5 +13,17 @@ test('Sum adds numbers', () => {
 test('subtract subtracts numbers', () => {
   const result = subtract(7, 3);
   const expected = 4;
+  expect(result).toBe(expected);
+});
+
+asyncTest('sumAsync adds numbers async', async () => {
+  const result = await sumAsync(4, 10);
+  const expected = 14;
+  expect(result).toBe(expected);
+});
+
+asyncTest('subtractAsync subtracts numbers async', async () => {
+  const result = await subtractAsync(10, 4);
+  const expected = 6;
   expect(result).toBe(expected);
 });
--- basics/testingFramework.js
+++ basics/testingFramework.js
@@ -9,6 +9,17 @@ function test(title, callback) {
   }
 }
 
+async function asyncTest(title, callback) {
+  try {
+    await callback();
+    console.log(`✔︎ ${title}`);
+  } catch (error) {
+    console.error(`✘ ${title}`);
+    console.log(error);
+  }
+}
+
 module.exports = {
-  test
+  test,
+  asyncTest
 };
