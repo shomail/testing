7be3090f04612dde641448aeb330ace3aa9e9c6e
Shomail <shomail1988@gmail.com>
2019-01-20 12:44:39 +0100

    eslint integration

---
+++ basics/utils.js
@@ -0,0 +1,13 @@
+const printName = (name = 'stranger') => {
+  const greeting = 'Hello';
+  const isType = typeof name === 'string';
+  if (isType) {
+    console.log(`${name} is valid name, say ${greeting}`);
+  } else {
+    console.log(`${name} is invalid!`);
+  }
+};
+
+module.exports = {
+  printName
+};
