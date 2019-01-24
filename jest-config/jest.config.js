7bd31ecc44c9b62acd576a5f73d47d004db6f55e
Shomail <shomail1988@gmail.com>
2019-01-24 22:31:52 +0100

    support webpack CSS modules in tests

--- jest-config/jest.config.js
+++ jest-config/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   //   testEnvironment: 'jest-environment-node',
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
+    '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
 }
