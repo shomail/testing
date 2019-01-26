70bec4f56737fa209b2d8e701b9ff6ecc4a9ee43
Shomail <shomail1988@gmail.com>
2019-01-26 14:26:19 +0100

    custom module resoulution support added in jest

--- jest-config/jest.config.js
+++ jest-config/jest.config.js
@@ -1,6 +1,9 @@
+const path = require('path')
+
 module.exports = {
   //   testEnvironment: 'jest-environment-node',
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
--- jest-config/src/__tests__/calculator.js
+++ jest-config/src/__tests__/calculator.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {render} from 'react-testing-library'
+import loadable from 'react-loadable'
 import Calculator from '../calculator'
 
-test('calculator renders', () => {
+test('calculator renders', async () => {
+  await loadable.preloadAll()
+  // const {container, debug} = render(<Calculator />)
+  // debug(container)
   render(<Calculator />)
 })
