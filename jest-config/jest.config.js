6b71bb9093261da0579e54026fed1c3a6ac776b3
Shomail <shomail1988@gmail.com>
2019-01-26 14:13:40 +0100

    setup afterEach test hook with jest

--- jest-config/jest.config.js
+++ jest-config/jest.config.js
@@ -5,4 +5,9 @@ module.exports = {
     '\\.module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  //two methods to load files:
+  //before jest is loaded
+  //   setupFiles: [],
+  //after jest is loaded
+  setupTestFrameworkScriptFile: require.resolve('./test/setup-tests.js'),
 }
