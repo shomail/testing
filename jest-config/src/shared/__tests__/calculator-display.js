ce760bbeeba4be7bcd34129fbc550a229c1c15a1
Shomail <shomail1988@gmail.com>
2019-01-29 21:30:38 +0100

    theming test in calculator display

--- jest-config/src/shared/__tests__/calculator-display.js
+++ jest-config/src/shared/__tests__/calculator-display.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import {render} from 'react-testing-library'
+import {ThemeProvider} from 'emotion-theming'
+import {dark} from '../../themes'
 import CalculatorDisplay from '../calculator-display'
 
 test('mounts', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
+  const {container} = render(
+    <ThemeProvider theme={dark}>
+      <CalculatorDisplay value="0" />
+    </ThemeProvider>,
+  )
   expect(container.firstChild).toMatchSnapshot()
 })
