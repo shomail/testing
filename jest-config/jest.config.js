2bf94b4a3639ab071411a75a2501d2111e019f21
Shomail <shomail1988@gmail.com>
2019-02-02 22:41:08 +0100

    run multiple configs with jest project feature

---
+++ jest-config/jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  ...require('./test/jest-common'),
+  projects: ['./test/jest.client.js', './test/jest.server.js'],
+  collectCoverageFrom: ['**/src/**/*.js'],
+  coverageThreshold: {
+    global: {
+      statements: 17,
+      branches: 4,
+      lines: 17,
+      functions: 20,
+    },
+    './src/shared/utils.js': {
+      statements: 100,
+      branches: 80,
+      lines: 100,
+      functions: 100,
+    },
+  },
+}
--- jest-config/test/jest.client.js
+++ jest-config/test/jest.client.js
@@ -1,19 +1,6 @@
 module.exports = {
   ...require('./jest-common'),
+  displayName: 'CLIENT',
   testEnvironment: 'jest-environment-jsdom',
   setupTestFrameworkScriptFile: require.resolve('./setup-tests.js'),
-  coverageThreshold: {
-    global: {
-      statements: 17,
-      branches: 4,
-      lines: 17,
-      functions: 20,
-    },
-    './src/shared/utils.js': {
-      statements: 100,
-      branches: 80,
-      lines: 100,
-      functions: 100,
-    },
-  },
 }
