e913bdbf0ffd7685c01eba2620a95a5494bb1b43
Shomail <shomail1988@gmail.com>
2019-02-05 22:42:06 +0100

    assert error message with test-id

--- react-test-app/src/__tests__/state.js
+++ react-test-app/src/__tests__/state.js
@@ -5,9 +5,13 @@ import {render, fireEvent} from 'react-testing-library'
 import {FavoriteNumber} from '../favourite-number'
 
 test('entering an invalid value shows an error message', () => {
-  const {getByLabelText, debug} = render(<FavoriteNumber />)
-  debug()
+  const {getByLabelText, getByTestId} = render(<FavoriteNumber />)
+  //   debug()
   const input = getByLabelText(/favorite number/i)
   fireEvent.change(input, {target: {value: 10}})
-  debug()
+  //   debug()
+  //   expect(container).toHaveTextContent(/the number is invalid/i)
+  expect(getByTestId('error-message')).toHaveTextContent(
+    /the number is invalid/i,
+  )
 })
