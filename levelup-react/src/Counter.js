ad884b3a0563b027fd26607052c5728fb3aabe89
Shomail <shomail1988@gmail.com>
2019-02-18 18:49:25 +0100

    Refactor Counter test to use testid for better node selection

--- levelup-react/src/Counter.js
+++ levelup-react/src/Counter.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
   };
 
   render() {
     const { count } = this.state;
     return (
       <div>
-        <button>{count}</button>
+        <button data-testid="counter-btn">{count}</button>
       </div>
     );
   }
--- levelup-react/src/Counter.test.js
+++ levelup-react/src/Counter.test.js
@@ -3,7 +3,11 @@ import { render, cleanup } from 'react-testing-library';
 import Counter from './Counter';
 
 test('<Counter />', () => {
-  const wrapper = render(<Counter />);
-  wrapper.debug();
-  expect(wrapper.getByText('0').tagName).toBe('BUTTON');
+  // render the component
+  const { debug, getByTestId } = render(<Counter />);
+  debug(); // output dom as string in test run
+
+  // Assert that we get the right element
+  expect(getByTestId('counter-btn').tagName).toBe('BUTTON');
+  expect(getByTestId('counter-btn').textContent).toBe('0');
 });
