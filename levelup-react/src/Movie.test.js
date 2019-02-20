8a331b53a85786c0f5ff2c70bb75db82157efa2e
Shomail <shomail1988@gmail.com>
2019-02-20 18:48:43 +0100

    test for error in Movie comp if no props provided

--- levelup-react/src/Movie.test.js
+++ levelup-react/src/Movie.test.js
@@ -4,6 +4,10 @@ import Movie from './Movie';
 
 afterEach(cleanup);
 
+console.error = jest.fn();
+
 test('<Movie />', () => {
+  // testing for error thrown if no props passed
   render(<Movie />);
+  expect(console.error).toBeCalled();
 });
