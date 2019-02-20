c964227c13eb453b7a8c9071272c377f6c6eac86
Shomail <shomail1988@gmail.com>
2019-02-20 19:11:10 +0100

    add test to verify render with movie prop

--- levelup-react/src/Movie.test.js
+++ levelup-react/src/Movie.test.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
+import { MemoryRouter } from 'react-router-dom';
 import Movie from './Movie';
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup();
+  // we need to clear mock function because in second test we are testing if it's not called
+  // but jest keep tracks of mocks and it will fail test because it was called previously
+  console.error.mockClear();
+});
 
 console.error = jest.fn();
+
+const movie = {
+  id: 'movie id',
+  title: 'Movie Title',
+  poster_path: 'poster_img.jpg',
+};
 
 test('<Movie />', () => {
   // testing for error thrown if no props passed
   render(<Movie />);
-  expect(console.error).toBeCalled();
+  expect(console.error).toHaveBeenCalled();
+});
+
+test('<Movie /> with Movie prop', () => {
+  // testing for error thrown if no props passed
+  render(
+    <MemoryRouter>
+      <Movie movie={movie} />
+    </MemoryRouter>,
+  );
+  expect(console.error).not.toBeCalled();
 });
