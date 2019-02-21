c776e197cf42876a2a2946c72761cc5c3cbe4b1d
Shomail <shomail1988@gmail.com>
2019-02-21 22:32:07 +0100

    mock fetch to test network request with fake data

--- levelup-react/src/MovieDetail.test.js
+++ levelup-react/src/MovieDetail.test.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { render, cleanup } from 'react-testing-library';
 import MovieDetail from './MovieDetail';
 
+global.fetch = require('jest-fetch-mock');
+
 afterEach(() => {
   cleanup();
   console.error.mockClear();
 });
 
+const match = {
+  params: {
+    id: 'movie id',
+  },
+};
+
 console.error = jest.fn();
 
 test('<MovieDetail />', () => {
-  // testing for error thrown if no props passed
-  const { debug } = render(<MovieDetail />);
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      movie: {
+        id: 'movie id',
+        title: 'movie title',
+      },
+    }),
+  );
+  const { debug } = render(<MovieDetail match={match} />);
   debug();
 });
