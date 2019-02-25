a027e20d3a521e5a4d59396418daa1f5fd8b5b34
Shomail <shomail1988@gmail.com>
2019-02-25 09:53:50 +0100

    refactor counter test

--- levelup-react/src/__tests__/Counter.test.js
+++ levelup-react/src/__tests__/Counter.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from 'react-testing-library';
-import Counter from './Counter';
+import Counter from '../Counter';
 
 afterEach(cleanup);
 
