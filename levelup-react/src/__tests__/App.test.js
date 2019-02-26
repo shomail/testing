1a6300f3d03b5d1066102e79bc8b5ea6d8743401
Shomail <shomail1988@gmail.com>
2019-02-26 17:59:12 +0100

    test App component rendering home url

--- levelup-react/src/__tests__/App.test.js
+++ levelup-react/src/__tests__/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, waitForElement } from 'react-testing-library';
 import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 global.fetch = require('jest-fetch-mock');
+
+const movies = {
+  success: true,
+  results: [
+    {
+      id: 123,
+      title: 'movie title 1',
+      poster_path: 'poster1.jpg'
+    },
+    {
+      id: 124,
+      title: 'movie title 2',
+      poster_path: 'poster2.jpg'
+    }
+  ]
+};
 
 afterEach(() => {
   cleanup();
@@ -12,11 +28,13 @@ afterEach(() => {
 
 console.error = jest.fn();
 
-test('<App />', () => {
-  const { debug } = render(
+test('<App />', async () => {
+  fetch.mockResponseOnce(JSON.stringify(movies));
+  const { debug, getByTestId } = render(
     <MemoryRouter>
       <App />
     </MemoryRouter>
   );
+  await waitForElement(() => getByTestId('movie-link'));
   debug();
 });
